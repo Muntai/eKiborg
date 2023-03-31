ca807cb40dc2e8784ed29bea0a54b4c650dec414
Мунтай <ruslan.marketing@gmail.com>
2023-03-31 23:27:31 +0300

    in home5

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 100);
+});
+
+
+// получаем элементы DOM
+const mainImg = document.querySelector(".img-container-product img");
+const thumbnailImgs = document.querySelectorAll(".hover-container-product img");
+
+// добавляем обработчики событий на эскизы
+thumbnailImgs.forEach((img) => {
+	img.addEventListener("click", () => {
+		// заменяем основное изображение на изображение, на которое кликнули
+		mainImg.src = img.src;
+	});
+});
+
+
 let darkmode = document.querySelector('#darkmode');
 let logo = document.querySelector('#logo');
 darkmode.onclick = () => {
@@ -10,7 +29,25 @@ darkmode.onclick = () => {
 		document.body.classList.remove('color');
 		logo.src = './img/logo-ekiborg-black.svg';
 	}
-}
+};
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('open');
+};
+
+const sr = ScrollReveal ({
+	distance: '70px',
+	duration: 2700,
+	reset: true
+});
 
 sr.reveal('.hero-text',{delay:200, origin: 'bottom'});
 sr.reveal('.hero-img',{delay:350, origin: 'top'});
@@ -25,3 +62,13 @@ sr.reveal ('.middle-text',{});
 sr.reveal ('.row-btn,.shop-content',{delay:300});
 
 //sr.reveal ('.contact',{delay:300});
+
+
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+	const scrollUp = document.getElementById('scroll-up');
+	// when the scroll is higher than 350 viewport height, add the show-scroll class to a tag with the scroll-top class
+	if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
